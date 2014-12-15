316edcafa0fa788b701e8966f1cbb6ebf0b71185
Alejandro Santiago <alemures@hotmail.com>
2014-12-15 10:11:52 +0000

    Added compareObject function to do a deep comparation between two different objects.

--- tests/test2.js
+++ tests/test2.js
@@ -1,6 +1,21 @@
 var ut = require("../lib/ut");
 
-var arr = ut.randomArray(1000000);
+var object1 = {
+    a: [1,2,3],
+    b: { c: 4, d: [5,6]},
+    e: 'Hello, World!',
+    f: [true, false],
+    g: { h: 2.332, i: 6e-4 }
+};
+
+var object2 = {
+    a: [1,2,3],
+    b: { c: 4, d: [5,6]},
+    e: 'Hello, World!',
+    f: [true, false],
+    g: { h: 2.332, i: 6e-4 }
+};
+
 ut.test(function() {
-    ut.sort(arr);
-}, 1);
+    console.log(ut.compareObject(object1, object2, true))
+}, 1, 'CompareObject');
