725fa540b3c5e2951e80af0d895abc535dd54a54
alejandro <you@example.com>
2015-07-12 17:10:52 +0100

    Added new functions and improved ut.logger, now it's able to configure the log level and enable or disable the date.

--- tests/test.js
+++ tests/test.js
@@ -5,68 +5,55 @@ if (typeof module !== 'undefined' && module.exports) {
 }
 
 if(isNode) {
-    var ut = require("../lib/ut");
+    var ut = require('../lib/ut');
 } else {
     // ut is accessible in the browser through window.ut
 }
 
-var lengthArr = 1000000;
-var times = 1;
-var printResult = false;
 
-var date1 = new Date();
-var date2 = new Date();
-
-var v0 = randomArr(lengthArr, false);
-var v1 = randomArr(lengthArr, false);
-var v2 = randomArr(lengthArr, false), v3 = randomArr(lengthArr, false);
-var v4 = randomArr(lengthArr, false);
-var v5 = randomArr(lengthArr, false);
-var v6 = randomArr(lengthArr, true);
-var v7 = randomArr(lengthArr, false);
-var v8 = randomArr(lengthArr, false);
-
-
-
-function randomArr(length, strings) {
-    strings = strings !== undefined ? strings : false;
-    var v = [], i;
-
-    for(i = 0; i < length; i++) {
-        if(strings) {
-            v.push(ut.randomString(ut.randomNumber(4, 25)));
-        } else {
-            v.push(ut.randomNumber(1, 1000000));
-        }
-    }
-    return v;
+var length = 10;
+function stringGenerator() {
+    return ut.randomString(ut.randomNumber(4, 25));
+}
+function numberGenerator() {
+    return ut.randomNumber(1, 1000000);
 }
 
-ut.test(function(){
+
+// Tests
+
+var times = 1;
+var printResult = true;
+
+var date1 = new Date();
+ut.test(function() {
     if(printResult) {
         console.log(ut.dateToMysql(date1));
     } else {
         ut.dateToMysql(date1);
     }
-},times, "dateToMysql()");
+}, times, 'dateToMysql()');
 
-
-ut.test(function(){
+var date2 = new Date();
+ut.test(function() {
     if(printResult) {
         console.log(ut.dateToString(date2));
     } else {
         ut.dateToString(date2);
     }
-},times, "dateToString()");
+},times, 'dateToString()');
 
+var v0 = ut.randomArray(length, numberGenerator);
+var chunkSize = 5;
 ut.test(function(){
     if(printResult) {
-        console.log(ut.arrayChunk(v0, ut.randomNumber(1, 15)));
+        console.log(ut.arrayChunk(v0, chunkSize));
     } else {
-        ut.arrayChunk(v0, ut.randomNumber(1, 15));
+        ut.arrayChunk(v0, chunkSize);
     }
-},times, "arrayChunk()");
+},times, 'arrayChunk()');
 
+var v1 = ut.randomArray(length, numberGenerator);
 ut.test(function(){
     if(printResult) {
         ut.sort(v1);
@@ -74,8 +61,10 @@ ut.test(function(){
     } else {
         ut.sort(v1);
     }
-},times, "sort()");
+},times, 'sort()');
 
+var v2 = ut.randomArray(length, numberGenerator);
+var v3 = ut.randomArray(length, numberGenerator);
 ut.test(function(){
     if(printResult) {
         ut.concatArrays(v2, v3);
@@ -83,16 +72,18 @@ ut.test(function(){
     } else {
         ut.concatArrays(v2, v3);
     }
-},times, "concatArrays()");
+},times, 'concatArrays()');
 
+var v4 = ut.randomArray(length, numberGenerator);
 ut.test(function(){
     if(printResult) {
         console.log(ut.copyArray(v4));
     } else {
         ut.copyArray(v4);
     }
-},times, "copyArray()");
+},times, 'copyArray()');
 
+var v5 = ut.randomArray(length, numberGenerator);
 ut.test(function(){
     if(printResult) {
         ut.clearArray(v5)
@@ -101,97 +92,178 @@ ut.test(function(){
         ut.clearArray(v5)
     }
 
-},times, "clearArray()");
+},times, 'clearArray()');
+
+function generator() {
+    return ut.randomNumber(1, 100);
+}
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.randomArray(length, generator));
+    } else {
+        ut.randomArray(length, generator)
+    }
+
+},times, 'randomArray()');
+
 
 ut.test(function(){
     if(printResult) {
         (function(a,b,c){
             var v = ut.argumentsToArray(arguments);
-            console.log("v[" + v + "], length = " + v.length);
+            console.log('v[' + v + '], length = ' + v.length);
         }(1,2,3));
     } else {
         (function(a,b,c){
             var v = ut.argumentsToArray(arguments);
         }(1,2,3));
     }
-},times, "argumentsToArray()");
+},times, 'argumentsToArray()');
+
+var stringLength = 100;
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.randomString(stringLength));
+    } else {
+        ut.randomString(stringLength);
+    }
+},times, 'randomString()');
+
+var randomString = ut.numberToString(ut.randomNumber(100, 1000000) / 2);
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.stringToNumber(randomString));
+    } else {
+        ut.stringToNumber(randomString);
+    }
+},times, 'stringToNumber()');
+
+var randomNumber = ut.randomNumber(1, 1000);
+var finalLength = 5;
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.padding(randomNumber, '-', finalLength));
+    } else {
+        ut.padding(randomNumber, '-', finalLength);
+    }
+},times, 'padding()');
+
+var randomNumber2 = ut.randomNumber(100, 1000000);
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.numberToString(randomNumber2));
+    } else {
+        ut.numberToString(randomNumber2);
+    }
+},times, 'numberToString()');
+
+var min = 25, max = 125;
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.randomNumber(min, max));
+    } else {
+        ut.randomNumber(min, max);
+    }
+},times, 'randomNumber()');
+
+var n1 = 5, n2 = 1, n3 = -1;
+ut.test(function(){
+    if(printResult) {
+        console.log('# All numbers ', n1, n2, n3);
+        console.log(ut.getMiddleNumber(n1, n2, n3));
+    } else {
+        ut.getMiddleNumber(n1, n2, n3);
+    }
+},times, 'getMiddleNumber()');
 
 ut.test(function(){
     if(printResult) {
-        console.log(ut.randomString(ut.randomNumber(10, 100)));
+        console.log(ut.randomObject([5,5,5]));
     } else {
-        ut.randomString(ut.randomNumber(10, 100));
+        ut.randomObject(5,5,5);
     }
-},times, "randomString()");
+},times, 'randomObject()');
+
+var randomObject = ut.randomObject([50]);
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.objectChunk(randomObject, 10));
+    } else {
+        ut.objectChunk(randomObject, 10);
+    }
+},times, 'objectChunk()');
+
+var object1 = {a:1, b: '2', c: false, d: [5,4,2], e: {f:5}};
+var object2 = {a:1, b: '2', c: false, d: [5,4,2], e: {f:6}};
+ut.test(function(){
+    if(printResult) {
+        console.log(ut.compareObject(object1, object2));
+    } else {
+        ut.compareObject(object1, object2);
+    }
+},times, 'compareObject()');
 
 ut.test(function(){
     if(printResult) {
-        console.log(ut.stringToNumber(ut.numberToString(ut.randomNumber(100, 1000000))));
+        console.log(ut.isNumeric('1.23'));
     } else {
-        ut.stringToNumber(ut.numberToString(ut.randomNumber(100, 1000000)));
+        ut.isNumeric('1.23');
     }
-},times, "stringToNumber()");
+},times, 'isNumeric()');
 
-ut.test(function(){
+ut.test(function() {
     if(printResult) {
-        console.log(ut.numberToString(ut.randomNumber(100, 1000000)));
+        console.log(ut.isNumber(4.23));
     } else {
-        ut.numberToString(ut.randomNumber(100, 1000000));
+        ut.isNumber(4.23);
     }
-},times, "numberToString()");
+},times, 'isNumber()');
 
-ut.test(function(){
+ut.test(function() {
     if(printResult) {
-        console.log(ut.prefix(ut.randomNumber(1, 10000), 5));
+        console.log(ut.isString('Alejandro'));
     } else {
-        ut.prefix(ut.randomNumber(1, 10000), 5);
+        ut.isString('Alejandro');
     }
-},times, "prefix()");
+},times, 'isString()');
 
-ut.test(function(){
-    if(printResult) {
-        console.log(ut.randomNumber(24, 1120));
-    } else {
-        ut.randomNumber(24, 1120);
-    }
-},times, "randomNumber()");
-
-ut.test(function(){
-    if(printResult) {
-        console.log(ut.getMiddleNumber(ut.randomNumber(100, 1000000), ut.randomNumber(100, 1000000), ut.randomNumber(100, 1000000)));
-    } else {
-        ut.getMiddleNumber(ut.randomNumber(100, 1000000), ut.randomNumber(100, 1000000), ut.randomNumber(100, 1000000));
-    }
-},times, "getMiddleNumber()");
-
-ut.test(function(){
-    if(printResult) {
-        console.log(ut.isNumber(ut.randomNumber(100, 1000000)));
-    } else {
-        ut.isNumber(ut.randomNumber(100, 1000000));
-    }
-},times, "isNumber()");
-
-ut.test(function(){
-    if(printResult) {
-        console.log(ut.isString(ut.randomString(ut.randomNumber(6, 100))));
-    } else {
-        ut.isString(ut.randomString(ut.randomNumber(6, 100)));
-    }
-},times, "isString()");
-
-ut.test(function(){
+var v7 = ut.randomArray(length, numberGenerator);
+ut.test(function() {
     if(printResult) {
         console.log(ut.isArray(v7));
     } else {
         ut.isArray(v7);
     }
-},times, "isArray()");
+},times, 'isArray()');
 
-ut.test(function(){
+ut.test(function() {
+    if(printResult) {
+        console.log(ut.isObject({}));
+    } else {
+        ut.isObject({});
+    }
+},times, 'isObject()');
+
+ut.test(function() {
+    if(printResult) {
+        console.log(ut.logN(2, 256));
+    } else {
+        ut.logN(2, 256);
+    }
+},times, 'logN()');
+
+var v8 = ut.randomArray(length, numberGenerator);
+ut.test(function() {
     if(printResult) {
         console.log(ut.inRange(v8, 800, 1200));
     } else {
         ut.inRange(v8, 800, 1200);
     }
-},times, "inRange()");
+},times, 'inRange()');
+
+ut.test(function() {
+    ut.logger.debug('hello');
+    ut.logger.info({a:2,b:'true'});
+    ut.logger.warn('Hello', 2, 'World');
+    ut.logger.error(new Error('This is an error'));
+},times, 'logger');
--- tests/test2.js
+++ tests/test2.js
@@ -1,16 +1,1 @@
-var ut = require("../lib/ut");
-var log = ut.logger;
-
-log.debug('hola');
-log.info(1);
-log.warn([true,1, '2']);
-log.error({a:true, b: 1, c: '2'});
-log.debug(false);
-log.debug();
-log.debug();
-log.debug([12,3]);
-log.debug(1,2,3,4, {a:true, b: 1, c: '2'}, [true,1, '2']);
-
-log.debug("The number is not correct", 1, "bye");
-
-log.error(new Error('Hola').stack);
+var ut = require('../lib/ut');
