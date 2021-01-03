04497e8ad9015c1b53e341e64f3555d834973c53
Alejandro Santiago <alemures@hotmail.com>
2021-01-03 17:28:51 +0100

    Memoized splitPath and added benchmarks

---
+++ benchmark/index.js
@@ -0,0 +1,126 @@
+const Benchmark = require('benchmark');
+const _ = require('lodash');
+const ut = require('../index');
+
+const suite = new Benchmark.Suite();
+
+// splitPath(suite);
+// stringToNumber(suite);
+// startsWith(suite);
+// getMiddleNumber(suite);
+// objectLength(suite);
+// objectLengthFastProperties(suite);
+// get(suite);
+
+suite.on('cycle', (event) => {
+  console.log(String(event.target));
+}).on('complete', () => {
+  console.log(`Fastest is ${suite.filter('fastest').map('name')}`);
+}).run({ async: true });
+
+function splitPath(suite) {
+  const path = 'a.b[2][2223332].zzxcsdf.ff[0].asf.ww[1][2][551].xx';
+  function splitPath(path) {
+    const objs = path.split('.');
+    let tokens = [];
+
+    for (let j = 0; j < objs.length; j++) {
+      const o = objs[j]
+      if (o.endsWith(']')) {
+        const i = o.indexOf('[');
+        const initialToken = o.substring(0, i)
+        tokens.push(initialToken)
+        const token = o.substring(i + 1, o.length - 1).split('][');
+        tokens = tokens.concat(token);
+      } else {
+        tokens.push(o)
+      }
+    }
+
+    return tokens;
+  }
+
+  suite.add('ut.splitPath', () => {
+    ut.splitPath(path);
+  }).add('custom splitPath', () => {
+    splitPath(path);
+  });
+}
+
+function stringToNumber(suite) {
+  const string = '25532';
+  suite.add('ut.stringToNumber', () => {
+    ut.stringToNumber(string);
+  }).add('custom stringToNumber', () => {
+    Number(string);
+  });
+}
+
+function startsWith(suite) {
+  const string = 'the beautiful string';
+  suite.add('ut.startsWith', () => {
+    ut.startsWith(string, 'the');
+  }).add('custom startsWith', () => {
+    string.startsWith('the');
+  });
+}
+
+function getMiddleNumber(suite) {
+  const a = 5, b = 3, c = 6;
+
+  function getMiddleNumber(a, b, c) {
+    if (a > b && b > c || c > b && b > a) return b;
+    if (b > a && a > c || c > a && a > b) return a;
+    return c;
+  }
+
+  suite.add('ut.getMiddleNumber', () => {
+    ut.getMiddleNumber(a, b, c);
+  }).add('custom getMiddleNumber', () => {
+    getMiddleNumber(a, b, c);
+  });
+}
+
+function objectLength(suite) {
+  const obj = ut.randomObject(125);
+
+  function objectLength(obj) {
+    return Object.keys(obj).length;
+  }
+
+  suite.add('ut.objectLength', () => {
+    ut.objectLength(obj);
+  }).add('custom objectLength', () => {
+    objectLength(obj);
+  });
+}
+
+function objectLengthFastProperties(suite) {
+  const obj = ut.toFastProperties(ut.randomObject(125));
+
+  function objectLength(obj) {
+    return Object.keys(obj).length;
+  }
+
+  suite.add('ut.objectLength', () => {
+    ut.objectLength(obj);
+  }).add('custom objectLength', () => {
+    objectLength(obj);
+  });
+}
+
+function get(suite) {
+  const obj = { a: { b: [1, 2, { c: true }] } };
+  const path = 'a.b[2].c';
+  const pathArr = ['a', 'b', '2', 'c'];
+
+  suite.add('ut.get string', () => {
+    ut.get(obj, path);
+  }).add('ut.get array', () => {
+    ut.get(obj, pathArr);
+  }).add('lodash get string', () => {
+    _.get(obj, path);
+  }).add('lodash get array', () => {
+    _.get(obj, pathArr);
+  });
+}
